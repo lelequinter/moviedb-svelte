fe7816345d836965a9b35cadac486800a8144aff
Leyder Quintero <sistemas78@red5g.co>
2023-06-30 09:10:30 -0500

    agregar Nav component al layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import global from '../global.css?inline';
+	import Nav from '../components/Nav.svelte';
 </script>
 
-<nav>
-	<ul>
-		<li>Hello</li>
-	</ul>
-</nav>
+<!--* Acá se agrega el SEO -->
+<svelte:head>
+	<title>Movie DB</title>
+</svelte:head>
+
+<Nav />
 
 <slot/>
